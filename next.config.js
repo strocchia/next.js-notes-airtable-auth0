2648c779080e5dc3614c7ba307b96edde8c5fdca
Scott Trocchia <scotttrocchia@gmail.com>
2022-09-06 20:36:49 -0400

    changed env in next config js again...

--- next.config.js
+++ next.config.js
@@ -7,9 +7,10 @@ const nextConfig = {
   swcMinify: true,
 
   env: {
-    // AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
-    // AIRTABLE_BASE_ID: process.env.AIRTABLE_BASE_ID,
-    // AIRTABLE_TABLE_NAME: process.env.AIRTABLE_TABLE_NAME,
+    NEXT_PUBLIC_AIRTABLE_API_KEY: process.env.NEXT_PUBLIC_AIRTABLE_API_KEY,
+    NEXT_PUBLIC_AIRTABLE_BASE_ID: process.env.NEXT_PUBLIC_AIRTABLE_BASE_ID,
+    NEXT_PUBLIC_AIRTABLE_TABLE_NAME:
+      process.env.NEXT_PUBLIC_AIRTABLE_TABLE_NAME,
   },
 };
 
