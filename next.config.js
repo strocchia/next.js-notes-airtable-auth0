3e5c50406656a46ca7b637df5c220c31d8111288
Scott Trocchia <scotttrocchia@gmail.com>
2022-09-06 20:20:38 -0400

    forgot next.config.js

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+
+  env: {
+    AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
+    AIRTABLE_BASE_ID: process.env.AIRTABLE_BASE_ID,
+    AIRTABLE_TABLE_NAME: process.env.AIRTABLE_TABLE_NAME,
+  },
+};
+
+module.exports = nextConfig;
